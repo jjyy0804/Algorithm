3e8a8fcae2340a8552b898a37dd013a9313bf852
jjyy0804 <87636861+jjyy0804@users.noreply.github.com>
2024-09-12 02:17:15 +0900

    [level 2] Title: 피보나치 수, Time: 4.77 ms, Memory: 38.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -0,0 +1,8 @@
+function solution(n) {
+  var fibonacci = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    fibonacci[i] = (fibonacci[i - 2] + fibonacci[i - 1]) % 1234567
+  }
+
+  return fibonacci[n];
+}
