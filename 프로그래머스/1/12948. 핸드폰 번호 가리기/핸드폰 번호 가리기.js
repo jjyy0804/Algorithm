373c9b1b5aef580824f50308dc03cdf53af18f15
jjyy0804 <87636861+jjyy0804@users.noreply.github.com>
2024-12-04 20:41:58 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,15 +1,13 @@
 function solution(phone_number) {
-    let answer = [];
-    let arr=phone_number.split('');
-    for(let i=0;i<arr.length;i++){
-        if(i<arr.length-4){
-        answer.push('*');     
-        }else{
-            answer.push(arr[i]);
-        }
-               
+  let answer = [];
+  let arr = phone_number.split('');
+  for (let i = 0; i < arr.length; i++) {
+    if (i < arr.length - 4) {
+      answer.push('*');
+    } else {
+      answer.push(arr[i]);
     }
-    
-    
-    return answer.join('');
+
+  }
+  return answer.join('');
 }
