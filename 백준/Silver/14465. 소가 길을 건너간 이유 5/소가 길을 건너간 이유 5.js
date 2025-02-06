2ebd1a550f53e2ab1752400174613e6736932799
jjyy0804 <87636861+jjyy0804@users.noreply.github.com>
2025-02-07 00:13:56 +0900

    [Silver II] Title: 소가 길을 건너간 이유 5, Time: 164 ms, Memory: 12108 KB -BaekjoonHub

---
+++ 백준/Silver/14465. 소가 길을 건너간 이유 5/소가 길을 건너간 이유 5.js
@@ -0,0 +1,24 @@
+let fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+const [N, K, B] = input[0].split(" ").map(Number);
+const broken_traffic_lights = new Set(input.slice(1).map(Number));
+
+const broken = Array(N + 1).fill(0);
+for (let light of broken_traffic_lights) {
+  broken[light] = 1;
+}
+
+const prefix = Array(N + 1).fill(0);
+for (let i = 1; i <= N; i++) {
+  prefix[i] = prefix[i - 1] + broken[i];
+}
+
+let min_repair = Infinity;
+for (let i = K; i <= N; i++) {
+  let broken_count = prefix[i] - prefix[i - K];
+  min_repair = Math.min(min_repair, broken_count);
+}
+
+console.log(min_repair);
