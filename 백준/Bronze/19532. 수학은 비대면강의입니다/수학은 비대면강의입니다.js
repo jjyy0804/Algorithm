ab703bf4eb422cd773e19e892b26b168af16eb09
이주영 <jjyy0804@naver.com>
2025-04-18 00:56:04 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 92 ms, Memory: 9584 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.js
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.js
@@ -8,6 +8,6 @@ const input = require("fs")
 const [a, b, c, d, e, f] = input;
 
 const x = parseInt((c * e - b * f) / (a * e - b * d));
-const y = parseInt((a * f - c * d) / (a * e - b * d))
+const y = parseInt((c * d - a * f) / (b * d - a * e))
 
 console.log(x, y);
