3eb0efa6508ca92a52fe0947a3ada1266ec07fdb
이주영 <jjyy0804@naver.com>
2025-05-07 20:24:21 +0900

    [Silver IV] Title: 차집합, Time: 916 ms, Memory: 237372 KB -BaekjoonHub

--- 백준/Silver/1822. 차집합/차집합.js
+++ 백준/Silver/1822. 차집합/차집합.js
@@ -1,14 +1,18 @@
-const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = fs.readFileSync(filePath).toString().trim().split("\n");
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "input.txt")
+  .toString()
+  .trim()
+  .split("\n");
 
-const [sizeA, sizeB] = input[0].split(" ").map(Number);
-const setA = new Set(input[1].split(" ").map(Number));
-const setB = new Set(input[2].split(" ").map(Number));
-
-const result = [...setA].filter(num => !setB.has(num)).sort((a, b) => a - b);
-
-console.log(result.length);
-if (result.length > 0) {
-    console.log(result.join(" "));
-}
+const [nA, nB] = input[0].split(" ").map(Number);
+const A = new Set(input[1].split(" ").map(Number));
+const B = new Set(input[2].split(" ").map(Number));
+let results = []
+A.forEach(num => {
+  if (!B.has(num)) {
+    results.push(num);
+  }
+})
+results.sort((a,b) => a - b);
+console.log(results.length);
+if (results.length > 0) console.log(results.join(" "));
