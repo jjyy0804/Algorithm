7d81314e51a9e5a3e1e1d7663d9809c5acdde0d5
이주영 <jjyy0804@naver.com>
2025-05-11 01:14:15 +0900

    [level 1] Title: 내적, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.js
+++ 프로그래머스/1/70128. 내적/내적.js
@@ -1,7 +1,7 @@
 function solution(a, b) {
-  var answer = 0;
-  for (let i = 0; i < a.length; i++) {
-    answer += a[i] * b[i]
-  }
-  return answer;
+    let result=0;
+    a.forEach((i,index)=>{
+        result+=i*b[index];
+    })
+    return result;
 }
