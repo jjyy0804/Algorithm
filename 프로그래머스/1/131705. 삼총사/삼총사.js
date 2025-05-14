bfb5830a9e88fd95db18f3d7b942bddb2612da08
이주영 <jjyy0804@naver.com>
2025-05-14 12:54:18 +0900

    [level 1] Title: 삼총사, Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.js
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -1,15 +1,26 @@
-function solution(number) {    
-    let result=0;
-    const dfs = (start,sum,count) =>{
-        if(count===3){
-            if(sum===0) result++;
-            return;            
+function solution(number) {
+    let result = 0;
+    let sum = 0;
+    let count = 0;
+
+    const dfs = (start) => {
+        if (count === 3) {
+            if (sum === 0) result++;
+            return;
         }
-        
-        for(let i = start ; i<number.length; i++){
-            dfs(i+1,sum+number[i],count+1);
+
+        for (let i = start; i < number.length; i++) {
+            sum += number[i];
+            count++;
+
+            dfs(i + 1);
+
+            // 백트래킹
+            sum -= number[i];
+            count--;
         }
-    }
-    dfs(0,0,0)
+    };
+
+    dfs(0);
     return result;
 }
