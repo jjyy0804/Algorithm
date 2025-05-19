a9a77b368dce4939a7da67136dd4f73d200e6d75
이주영 <jjyy0804@naver.com>
2025-05-19 18:40:40 +0900

    [level 1] Title: 모의고사, Time: 32.60 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,23 +1,23 @@
 function solution(answers) {
-    const a = [1,2,3,4,5]
+    const a = [1,2,3,4,5];
     const b = [2,1,2,3,2,4,2,5];
     const c = [3,3,1,1,2,2,4,4,5,5];
-    let aCount =0;
-    let bCount =0;
-    let cCount =0;
     
-    for(let i =0 ; i<answers.length;i++){
-        if(a[(i%5)]===answers[i]) aCount++;
-        if(b[(i%8)]===answers[i]) bCount++;
-        if(c[(i%10)]===answers[i]) cCount++;
+    const counts=[0,0,0]
+    
+    for(let i =0;i<answers.length;i++){
+        if(answers[i]===a[i%5]) counts[0]++;
+        if(answers[i]===b[i%8]) counts[1]++;
+        if(answers[i]===c[i%10]) counts[2]++;
     }
     
-    const maxScore = Math.max(aCount, bCount, cCount);
-    const result = [];
-
-    if (aCount === maxScore) result.push(1);
-    if (bCount === maxScore) result.push(2);
-    if (cCount === maxScore) result.push(3);
+    const maxCount = Math.max(...counts);
+    const results=[];
     
-    return result;
+    for(let i = 0;i<counts.length;i++){
+        if(counts[i]===maxCount){
+            results.push(i+1);
+        }
+    }
+    return results;
 }
