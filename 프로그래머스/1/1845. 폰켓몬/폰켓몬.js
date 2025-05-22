03db68755da42f4f4054b502080fc48012ac4842
이주영 <jjyy0804@naver.com>
2025-05-22 12:58:10 +0900

    [level 1] Title: 폰켓몬, Time: 0.55 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,11 +1,6 @@
 function solution(nums) {
-  var answer = 0;
-  let myPonketmon = nums.length/2;
-  let filtered = [...new Set(nums)];
-  if (filtered.length > myPonketmon) {
-    answer = myPonketmon;
-  } else {
-    answer = filtered.length;
-  }
-  return answer;
+    const removeDuplication = new Set(nums);
+    const count = (nums.length)/2;
+    
+    return count>removeDuplication.size?removeDuplication.size:count;
 }
